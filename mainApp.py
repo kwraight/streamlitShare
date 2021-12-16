c28db5bd25ca219a09b6b8fe4c749980be6ed362
Kenneth Gibb Wraight <kenneth.gibb.wraight@cern.ch>
2021-12-16 23:03:12 +0000

    remove duplicate main file

--- mainApp.py
+++ mainApp.py
@@ -2,4 +2,4 @@ from core.App import App
 
 myapp = App("stTemp", "Submission builder v1.0")
 
-myapp.main() 
+myapp.main()
--- streamlit_app.py
+++
@@ -1,5 +0,0 @@
-from core.App import App
-
-myapp = App("stTemp", "Submission builder v1.0")
-
-myapp.main() 
